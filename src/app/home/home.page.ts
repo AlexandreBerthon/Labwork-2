a2e83b2b2d81f243700e3f2895ebb2265f9ed89a
Alexandre Berthon <berthon.alex@gmail.com>
2019-03-05 12:18:00 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform, ActionSheetController } from '@ionic/angular'
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(
+    public platform: Platform,
+    public actionSheetCtrl: ActionSheetController
+  ){}
+
+  async openMenu(){
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Albums',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: ()=> {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Share',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: ()=> {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Play',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: ()=> {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: ()=> {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: ()=> {
+            console.log('Cancel clicked');
+          }
+        },
+      ]
+    });
+    actionSheet.present();
+  }
 
 }
